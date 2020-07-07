405971c66c97c96e0b1e83d501cb0b06af13d021
Nozomi Hosaka <nozomi.hosaka@smaregi.jp>
2020-07-07 13:51:05 +0900

    取得した契約ID通知をWebhookログに登録するようにしました。

--- src/app/Http/Controllers/Actions/SmaregiApiToken/SmaregiApiTokenAcceptAction.php
+++ src/app/Http/Controllers/Actions/SmaregiApiToken/SmaregiApiTokenAcceptAction.php
@@ -4,17 +4,45 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Actions\SmaregiApiToken;
 
 use App\Http\Controllers\Controller;
+use App\Models\WebhookLogs;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Log;
+use Str;
 
 class SmaregiApiTokenAcceptAction extends Controller
 {
     /**
+     * @var WebhookLogs
+     */
+    private $webhookLogs;
+
+    /**
+     * SmaregiApiTokenAcceptAction constructor.
+     *
+     * @param WebhookLogs $webhookLogs
+     */
+    public function __construct(WebhookLogs $webhookLogs)
+    {
+        $this->webhookLogs = $webhookLogs;
+    }
+
+    /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return JsonResponse
      */
     public function __invoke(Request $request)
     {
+        $webhookLogs = $this->webhookLogs->newQuery()
+            ->create([
+                'id' => Str::uuid(),
+                'header' => json_encode($request->headers->all(), JSON_UNESCAPED_SLASHES),
+                'body' => json_encode($request->all(), JSON_UNESCAPED_SLASHES),
+            ]);
+
+        Log::info($webhookLogs->toJson());
+        return response()->json([], 204);
     }
 }
